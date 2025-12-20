f40e13a7675fcd77a54955593f1ea03917dfef33
Rafael Mota Moraes Moreira <rafaelmota622@gmail.com>
2025-12-20 15:56:36 -0300

    template

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'recipes'
+
 urlpatterns = [
-    path('', views.home),  # home
+    path('', views.home, name='home'),
+    path('recipes/<int:id>/', views.recipe, name='recipe'),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse, HttpRequest
 
+from recipes.utils import make_recipe
+
 
 def home(request: HttpRequest) -> HttpResponse:
     return render(request, 'recipes/pages/home.html', {
-        'nome': 'Rafael'
+        'recipes': [make_recipe() for _ in range(10)]
+    })
+
+
+def recipe(request: HttpRequest, id: int) -> HttpResponse:
+    return render(request, 'recipes/pages/recipe-view.html', {
+        'recipe': make_recipe(),
+        'is_detail_page': True
     })
