8ce313fcd8ed5dbcba679946d7631942ce7f4979
Rafael Mota Moraes Moreira <rafaelmota622@gmail.com>
2025-12-20 14:15:34 -0300

    urls, view e templates

---
+++ recipes/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse, HttpRequest
+
+
+def home(request: HttpRequest) -> HttpResponse:
+    return render(request, 'recipes/pages/home.html', {
+        'nome': 'Rafael'
+    })
