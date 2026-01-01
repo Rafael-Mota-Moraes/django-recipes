aeb2b63cc0a7968cb67fb0206b724ae3a2bc6a68
Rafael Mota Moraes Moreira <rafaelmota622@gmail.com>
2026-01-01 13:20:58 -0300

    Models e ORM

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Recipe
 
-# Register your models here.
+
+@admin.register(Recipe)
+class RecipeAdmin(admin.ModelAdmin):
+    pass
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+
+admin.site.register(Category, CategoryAdmin)
--- recipes/views.py
+++ recipes/views.py
@@ -1,17 +1,31 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpRequest
+from django.shortcuts import render, get_list_or_404, get_object_or_404
+from django.http import HttpResponse, HttpRequest, Http404
 
 from recipes.utils import make_recipe
+from recipes.models import Recipe
 
 
 def home(request: HttpRequest) -> HttpResponse:
+    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
     return render(request, 'recipes/pages/home.html', {
-        'recipes': [make_recipe() for _ in range(10)]
+        'recipes': recipes
+    })
+
+
+def category(request: HttpRequest, category_id) -> HttpResponse:
+    recipes = get_list_or_404(Recipe.objects.filter(
+        category__id=category_id, is_published=True).order_by('-id'))
+
+    return render(request, 'recipes/pages/category.html', {
+        'recipes': recipes,
+        'title': f'{recipes[0].category.name}'  # type: ignore
     })
 
 
 def recipe(request: HttpRequest, id: int) -> HttpResponse:
+    recipe = get_object_or_404(Recipe.objects.filter(pk=id, is_published=True))
+
     return render(request, 'recipes/pages/recipe-view.html', {
-        'recipe': make_recipe(),
+        'recipe': recipe,
         'is_detail_page': True
     })
