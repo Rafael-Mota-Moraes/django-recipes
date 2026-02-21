a588cbf3a1c16805e76f81efebaec742595872f0
Rafael Mota Moraes Moreira <rafaelmota622@gmail.com>
2026-02-21 10:54:19 -0300

    added admin area

--- recipes/admin.py
+++ recipes/admin.py
@@ -4,7 +4,17 @@ from .models import Category, Recipe
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id', 'title', 'created_at', 'is_published', 'author')
+    list_display_links = ('id', 'title', 'created_at',)
+    search_fields = ('id', 'title', 'description', 'slug', 'preparation_steps')
+    list_filter = ('category', 'author', 'is_published',
+                   'preparation_steps_is_html')
+    list_per_page = 10
+    list_editable = ('is_published',)
+    ordering = ('-id', )
+    prepopulated_fields = {
+        'slug': ('title',)
+    }
 
 
 class CategoryAdmin(admin.ModelAdmin):
